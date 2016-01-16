a96f7bb6a61f0dd0da874c0474aa84094c9cd11c
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-01-16 12:06:30 +0200

    some formatting

--- main.go
+++ main.go
@@ -4,18 +4,24 @@ import (
 	"log"
 	"github.com/mitchellh/colorstring"
 	"fmt"
+	"os"
 )
 
 func main() {
 
-	resp, err := search("log")
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "usage: go-pkg sql\n")
+		os.Exit(1)
+	}
+
+	resp, err := search(os.Args[1])
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	for _, element := range resp.Results {
-		str := colorstring.Color(fmt.Sprintf("[red] Name [blue]%s", element.Name))
+		str := colorstring.Color(fmt.Sprintf("[red] Name\n [blue]%s", element.Name))
 		colorstring.Println(str)
 	}
 }
